41a80bd1cd0771f0960e89a4d4da82ab5eba1d8f
Fraga <0331518@students.dadeschools.net>
2019-04-16 10:21:17 -0400

    created questions

---
+++ main.js
@@ -0,0 +1,24 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30;
+
+
+//Questions set
+var questions = [
+    
+    {
+    questions:"What was Hannah Montana's real identity?"
+    answers: ["Miley,Lilly,Mia,Laura"],
+    correctAnswer: "Miley",
+    image: 
+    },
+
+
+    {
+        questions:"Who was not a member of the wild cats?"
+        answers: ["Chase,Troy,Ryan,Zeke"],
+        correctAnswer: "Ryan",
+        image:  
+        },
+    
+]
