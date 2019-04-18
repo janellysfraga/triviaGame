5c3589af401b04a5f2ec58b0294bf063b0cd850e
Fraga <0331518@students.dadeschools.net>
2019-04-18 10:09:55 -0400

    added more functions

--- main.js
+++ main.js
@@ -7,18 +7,53 @@ var countStartNumber = 30;
 var questions = [
     
     {
-    questions:"Who did Ross date in the well known sitcom friends?"
-    answers: ["Hellen, Phoebe, Monica, Rachel"],
-    correctAnswer: "Rachel",
-    image: assests\ross and rachel 2.jpg
+    questions:"What was Hannah Montana's real identity?"
+    answers: ["Miley,Lilly,Mia,Laura"],
+    correctAnswer: "Miley",
+    image: 
     },
 
 
     {
-        questions:"Who was not a member of the wild cats in the movie Highschool Musical?"
+        questions:"Who was not a member of the wild cats?"
         answers: ["Chase,Troy,Ryan,Zeke"],
         correctAnswer: "Ryan",
-        image:  assests\wild cats.jpg
+        image: 
         },
     
 ]
+    // Variable to hold our setInterval
+    var timer;
+
+    var game = {
+
+        questions: questions,
+        currentQuestion = 0,
+        Counter: countStartNumber,
+        correct: 0,
+        incorrect: 0,
+
+    countdown: function() {
+
+        game.correct--;
+        $("#counter-number").html(game.counter);
+        if (game.counter === 0) {
+            console.log('TIME UP');
+            game.timeUp();
+        }
+
+    }
+
+    loadQuestion: function() {
+
+        timer = set setInterval(game.counter, 1000);
+        panel.html("<h2>"+ questions[this.currentQuestion].questions +"</h2>");
+        for (var i = 0; i < questions[this.currentQuestion].answers.legnth; i++)
+        panel.append("<button class='answer-button' id ='button' deta-name =''" + questions[this.currentQuestion].answers[i]) + "'>'" + questions[this.currentQuestion].answer[i] + "</button>";
+    )
+
+    };
+
+
+
+}
