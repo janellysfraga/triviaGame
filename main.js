06007765df73c8ec9d8f35cdadde05894b7894fa
Fraga <0331518@students.dadeschools.net>
2019-04-18 20:43:49 -0400

    Edited the mistakes

--- main.js
+++ main.js
@@ -2,57 +2,67 @@ var panel = $("#quiz-area");
 
 var countStartNumber = 30;
 
-
-//Questions set
+// Question set
 var questions = [
     
     {
-    questions:"What was Hannah Montana's real identity?"
-    answers: ["Miley,Lilly,Mia,Laura"],
-    correctAnswer: "Miley",
-    image: 
+        question: "Who dated Ross in the well-known sitcom friends?",
+        answers: ["Rachel", "Monica", "Phoebe", "Emily"],
+        correctAnswer: "Rachel",
+        image: "assests\ross and rachel 2.jpg"
     },
 
-
     {
-        questions:"Who was not a member of the wild cats?"
-        answers: ["Chase,Troy,Ryan,Zeke"],
+        question: "Who was not a member of the wild cats in the movie High School Musical?",
+        answers: ["Chase", "Troy", "Ryan", "Zeke"],
         correctAnswer: "Ryan",
-        image: 
-        },
-    
-]
-    // Variable to hold our setInterval
-    var timer;
-
-    var game = {
-
-        questions: questions,
-        currentQuestion = 0,
-        Counter: countStartNumber,
-        correct: 0,
-        incorrect: 0,
-
-    countdown: function() {
-
-        game.correct--;
-        $("#counter-number").html(game.counter);
-        if (game.counter === 0) {
-            console.log('TIME UP');
-            game.timeUp();
-        }
-
+        image: "assests\wild cats.jpg"
     }
 
+
+]
+
+// Variable to hold our setInterval
+var timer;
+
+var game = {
+
+    questions: questions,
+    currentQuestion: 0,
+    counter: countStartNumber,
+    correct: 0,
+    incorrect: 0,
+
+    countdown: function() {
+        game.counter--;
+        $("#counter-number").html(game.counter);
+        if (game.counter === 0) {
+            console.log("TIME UP");
+            game.timeUp();
+        }
+    },
+
     loadQuestion: function() {
+        timer = setInterval(game.countdown, 1000);
+        panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
+        for (var i = 0; i < questions[this.currentQuestion].answers.length; i++)
+        {
+            panel.append("<button class='answer-button' id='button' data-name=' " +
+            questions[this.currentQuestion].answer[i] +
+            "'>" + questions[this.currentQuestion].answers[i] + "</button>");
+        }
+    },
 
-        timer = set setInterval(game.counter, 1000);
-        panel.html("<h2>"+ questions[this.currentQuestion].questions +"</h2>");
-        for (var i = 0; i < questions[this.currentQuestion].answers.legnth; i++)
-        panel.append("<button class='answer-button' id ='button' deta-name =''" + questions[this.currentQuestion].answers[i]) + "'>'" + questions[this.currentQuestion].answer[i] + "</button>";
-    )
+    nextQuestion: function() {
+        game.counter = countStartNumber;
+        $("#counter-number").html(game.counter);
+        game.currentQuestion++;
+        game.loadQuestion();
+    },
 
-    };
+    timeUp: function() {
+        
+    }
 
 
 
