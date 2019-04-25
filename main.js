d91e866240acbace50c64331a43f890cb20641fe
Fraga <0331518@students.dadeschools.net>
2019-04-25 09:48:32 -0400

    finished objects

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ var game = {
         {
             setTimeout(game.nextQuestion, 3 * 1000);
         }
-    }
+    },
 
     results: function() {
 
@@ -91,6 +91,70 @@ var game = {
         panel.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
         panel.append("<h3>Unanswered: " + (questions.length - (game.correct + game.incorrect)) + "</h3>");
         panel.append("<br><button id= 'start over'>Start Over?</button>");
+    },
+
+    clicked: function(e) {
+
+        clearInterval(timer);
+
+        if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer)
+        {
+            this.answeredCorrectly();
+        }
+        else
+        {
+            this.answeredIncorrectly();
+        }
+
+    },
+
+    answeredIncorrectly: function() {
+
+        game.incorrect++;
+
+        clearInterval(timer);
+
+        panel.html("<h2>Nope!</h2>");
+        panel.append("<h3>The correct answer was: " + questions[game.currentQuestion].correctAnswer + "</h3>");
+        panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
+
+        if (game.currentQuestion === questions.length - 1)
+        {
+            setTimeout(game.results, 3 * 1000);
+        }
+        else
+        {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+
+    },
+
+    answeredCorrectly: function() {
+
+        game.correct++;
+
+        clearInterval(timer);
+
+        panel.html("<h2>Correct!</h2>");
+        panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
+
+        if (game.currentQuestion === questions.length - 1)
+        {
+            setTimeout(game.results, 3 * 1000);
+        }
+        else
+        {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+    },
+
+    reset: function() {
+
+        this.currentQuestion = 0;
+        this.counter = countStartNumber;
+        this.correct = 0;
+        this.incorrect = 0;
+        this.loadQuestion();
     }
 
-}
+};
