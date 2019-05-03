01b93f99388963b75ad21825dc1c71cc9e60eb05
Fraga <0331518@students.dadeschools.net>
2019-05-03 10:29:52 -0400

    done but need to fix erros

--- main.js
+++ main.js
@@ -158,3 +158,25 @@ var game = {
     }
 
 };
+
+
+// CLICK EVENTS
+// --------------------------------------
+$(document).on("click", "#start-over", function() {
+
+    game.reset();
+
+});
+
+ $(document).on("click", ".answer-button", function(e) {
+
+    game.clicked(e);
+
+ });
+
+ $(document).on("click", "#start", function(){
+
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id ='counter-number'>30</span>Seconds</h2>");
+    game.loadQuestion();
+
+ });
