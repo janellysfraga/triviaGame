1ea5fe04aeeaf0704f8e63c39fe57321cfee771e
Fraga <0331518@students.dadeschools.net>
2019-05-03 20:56:46 -0400

    fixed errors

--- main.js
+++ main.js
@@ -14,11 +14,10 @@ var questions = [
 
     {
         question: "Who was not a member of the wild cats in the movie High School Musical?",
-        answers: ["Chase", "Troy", "Ryan", "Zeke"],
+        answers: ["Chase", "Ryan", "Zeke"],
         correctAnswer: "Ryan",
         image: "assests\wild cats.jpg"
     }
-
 
 ]
 
@@ -47,8 +46,8 @@ var game = {
         panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
         for (var i = 0; i < questions[this.currentQuestion].answers.length; i++)
         {
-            panel.append("<button class='answer-button' id='button' data-name=' " +
-            questions[this.currentQuestion].answer[i] +
+            panel.append("<button class='answer-button' id='button' data-name='" +
+            questions[this.currentQuestion].answers[i] +
             "'>" + questions[this.currentQuestion].answers[i] + "</button>");
         }
     },
@@ -65,9 +64,10 @@ var game = {
         clearInterval(timer);
 
         $("#counter-number").html(game.counter);
+
         panel.html("<h2>Out of Time!</h2>");
-        panel.append("<h3> The Correct answer was: " + questions[this.currentQuestion].correctAnswer);
-        panel.append("<img src= '" + questions[this.currentQuestion].image + "'/>");
+        panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
+        panel.append("<img src='" + questions[this.currentQuestion].image + "'/>" );
 
         if (game.currentQuestion === questions.length - 1)
         {
@@ -77,6 +77,7 @@ var game = {
         {
             setTimeout(game.nextQuestion, 3 * 1000);
         }
+
     },
 
     results: function() {
@@ -90,14 +91,14 @@ var game = {
         panel.append("<h3>Correct Answers: " + game.correct + "</h3>");
         panel.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
         panel.append("<h3>Unanswered: " + (questions.length - (game.correct + game.incorrect)) + "</h3>");
-        panel.append("<br><button id= 'start over'>Start Over?</button>");
+        panel.append("<br><button id='start-over'>Start Over?</button>");
+        
     },
 
     clicked: function(e) {
 
         clearInterval(timer);
-
-        if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer)
+        if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) 
         {
             this.answeredCorrectly();
         }
@@ -115,8 +116,8 @@ var game = {
         clearInterval(timer);
 
         panel.html("<h2>Nope!</h2>");
-        panel.append("<h3>The correct answer was: " + questions[game.currentQuestion].correctAnswer + "</h3>");
-        panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
+        panel.append("<h3>The Correct Answer was: " + questions[game.currentQuestion].correctAnswer + "</h3>");
+        panel.append("<img src='" + questions[game.currentQuestion].image + "'/>" );
 
         if (game.currentQuestion === questions.length - 1)
         {
@@ -136,7 +137,7 @@ var game = {
         clearInterval(timer);
 
         panel.html("<h2>Correct!</h2>");
-        panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
+        panel.append("<img src='" + questions[game.currentQuestion].image + "'/>" );
 
         if (game.currentQuestion === questions.length - 1)
         {
@@ -146,6 +147,7 @@ var game = {
         {
             setTimeout(game.nextQuestion, 3 * 1000);
         }
+
     },
 
     reset: function() {
@@ -155,28 +157,23 @@ var game = {
         this.correct = 0;
         this.incorrect = 0;
         this.loadQuestion();
+
     }
 
 };
 
 
 // CLICK EVENTS
-// --------------------------------------
+// ----------------------------------------------------------------
 $(document).on("click", "#start-over", function() {
-
     game.reset();
-
 });
 
- $(document).on("click", ".answer-button", function(e) {
-
+$(document).on("click", ".answer-button", function(e) {
     game.clicked(e);
+});
 
- });
-
- $(document).on("click", "#start", function(){
-
-    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id ='counter-number'>30</span>Seconds</h2>");
+$(document).on("click", "#start", function() {
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30 </span> Seconds</h2>");
     game.loadQuestion();
-
- });
+});
